16be8cf8fcd941ff59fae9680df0f5ac9a6b5aa6
“reza <rezakhiabani80@gmail.com>
2021-05-19 15:03:53 +0430

    add function

--- app/src/main/java/com/example/projecttestsendgit/MainActivity.java
+++ app/src/main/java/com/example/projecttestsendgit/MainActivity.java
@@ -11,8 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this, "reza", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "reza", Toast.LENGTH_SHORT).show();
-// sallam
+        casting();
     }
+
+private void casting(){
+	Toast
+}
 }
